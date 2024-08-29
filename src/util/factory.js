3901197857c6b04babc31df820c4d9d2b8234a72
NPLM <putiline@gmail.com>
2024-08-29 15:03:39 +0300

    test2

--- src/util/factory.js
+++ src/util/factory.js
@@ -326,6 +326,7 @@ const Factory = function () {
 
     const domainName = DomainName(window.location.search.substring(1))
     console.log(window.location.href + '/?' + window.location.href + '/files/techradar.json')
+    console.log(domainName + '/files/techradar.json')
     //sheet = JSONFile(window.location.href + '?' + window.location.href + '/files/techradar.json')
     //sheet.init().build()
 
